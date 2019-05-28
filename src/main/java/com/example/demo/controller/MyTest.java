203fbc9f484a65f1becc66742ff6021bc5d4758e
xudong222 <15135486424@163.com>
2019-05-29 00:09:59 +0800

    waedrftgyhjk

--- src/main/java/com/example/demo/controller/MyTest.java
+++ src/main/java/com/example/demo/controller/MyTest.java
@@ -5,5 +5,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("6666");
+        System.out.println("白慧敏大傻子");
     }
 }
